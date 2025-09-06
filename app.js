ed5670dd84cf14a8c61cf68ac0982be59bde2059
TaMagolor <friendly.taisho@gmail.com>
2025-09-06 19:58:36 +0900

    v.3.4.7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // =================================================================
 // Application Version
 // =================================================================
-const APP_VERSION = 'v.3.4.6'; // Fixed lyrics UI and settings panel bugs
+const APP_VERSION = 'v.3.4.7'; // Fixed lyrics UI and settings panel bugs
 
 // =================================================================
 // HTML Element Acquisition
