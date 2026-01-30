b3871d789de251344e824587c9b1a09619560b17
TaMagolor <friendly.taisho@gmail.com>
2026-01-31 02:39:34 +0900

    v.5.0.6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // =================================================================
 // Application Version
 // =================================================================
-const APP_VERSION = 'v.5.0.5'; // Fixed property panel UI bugs
+const APP_VERSION = 'v.5.0.6'; // Fixed property panel UI bugs
 
 // =================================================================
 // HTML Element Acquisition
